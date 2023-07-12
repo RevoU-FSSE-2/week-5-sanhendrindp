6e10c8435f16f9c4926dd5c79554913207cf67c4
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2023-07-13 03:51:02 +0700

    pre loader created

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 "use strict";
 
+// LOADING SCREEN
+const loader = document.querySelector(".loader");
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    loader.classList.add("loader-hidden");
+  }, 3000);
+});
+
+// HAMBURGER MENU OPEN/CLOSE
 const navbar = document.querySelector(".navbar");
 const menuBtn = document.querySelector("#menu");
 
