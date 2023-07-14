836a66c3963780c94bdbb8de7403e6e33e15717e
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2023-07-14 23:11:09 +0700

    finishing

--- script.js
+++ script.js
@@ -38,11 +38,38 @@ document.querySelector(".popup-gallery i").onclick = function () {
   document.querySelector("nav").style.display = "flex";
 };
 
-// TYPED JS
-// const typed = new Typed(".multiple-text", {
-//   strings: ["IMAGE & DATA TAGGING", "CATEGORIZATION", "AUDIT"],
-//   typeSpeed: 100,
-//   backSpeed: 100,
-//   backDelay: 2000,
-//   loop: true,
-// });
+// FORM CONTACT
+// var form = document.getElementById("my-form");
+
+// async function handleSubmit(event) {
+//   event.preventDefault();
+//   var status = document.getElementById("my-form-status");
+//   var data = new FormData(event.target);
+//   fetch(event.target.action, {
+//     method: form.method,
+//     body: data,
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   })
+//     .then((response) => {
+//       if (response.ok) {
+//         status.innerHTML = "Thanks for your submission!";
+//         form.reset();
+//       } else {
+//         response.json().then((data) => {
+//           if (Object.hasOwn(data, "errors")) {
+//             status.innerHTML = data["errors"]
+//               .map((error) => error["message"])
+//               .join(", ");
+//           } else {
+//             status.innerHTML = "Oops! There was a problem submitting your form";
+//           }
+//         });
+//       }
+//     })
+//     .catch((error) => {
+//       status.innerHTML = "Oops! There was a problem submitting your form";
+//     });
+// }
+// form.addEventListener("submit", handleSubmit);
